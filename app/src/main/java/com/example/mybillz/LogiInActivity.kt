437a5f00d82b34b04367aaed15c6c2a1afd8af62
Marion <njeriwmarion@gmail.com>
2023-06-23 16:39:03 +0300

    created signup and login pages

--- app/src/main/java/com/example/mybillz/LogiInActivity.kt
+++ app/src/main/java/com/example/mybillz/LogiInActivity.kt
@@ -2,10 +2,47 @@ package com.example.mybillz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.mybillz.databinding.ActivityLogiInBinding
+import com.example.mybillz.databinding.ActivityMainBinding
 
 class LogiInActivity : AppCompatActivity() {
+    lateinit var binding: ActivityLogiInBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding= ActivityLogiInBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_logi_in)
+        validatLog()
+        clear()
+
     }
+
+    override fun onResume() {
+        super.onResume()
+        setContentView(binding.root)
+    }
+    fun validatLog(){
+        val name1=binding.etUserName.text.toString()
+        val name2=binding.etPassword.text.toString()
+
+        var error = true
+
+        if(name1.isBlank()){
+            binding.tilUserName.error="User name is required"
+            error=false
+        }
+        if(name2.isBlank()){
+            binding.tilPassword.error="Password required"
+        }
+        if(!error){
+            Toast.makeText(this, "You're logged in", Toast.LENGTH_SHORT).show()
+            finish()
+        }
+    }
+    fun clear(){
+        binding.tilUserName.error=null
+        binding.tilPassword.error=null
+    }
+
 }
--- app/src/main/java/com/example/mybillz/MainActivity.kt
+++ app/src/main/java/com/example/mybillz/MainActivity.kt
@@ -1,11 +1,73 @@
 package com.example.mybillz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.mybillz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+        validateregistration()
+
     }
+
+    override fun onResume() {
+        super.onResume()
+        setContentView(binding.root)
+        binding.btnSignUp.setOnClickListener {
+            clearErrors()
+        }
+        binding.tvSignIn.setOnClickListener {
+            val intent=Intent(this, LogiInActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    fun validateregistration() {
+        val name = binding.etUserName.text.toString()
+        val name1 = binding.etEmail.text.toString()
+        val name2 = binding.etPhoneNumber.text.toString()
+        val name3 = binding.etPassword.text.toString()
+
+        var error = false
+
+        if (name.isBlank()){
+            binding.tilUserName.error="Username required"
+            error=true
+        }
+        if(name1.isBlank()){
+            binding.tilEmail.error="Email required"
+            error=true
+        }
+        if(name2.isBlank()){
+            binding.tilPhoneNumber.error="PhoneNumber required"
+            error=true
+        }
+        if(name3.isBlank()){
+            binding.tilPassword.error="Password is required"
+            error=true
+        }
+        if(!error){
+            val intent=Intent(this, LogiInActivity::class.java)
+            startActivity(intent)
+            Toast.makeText(this, "Registration is completed",Toast.LENGTH_SHORT).show()
+            finish()
+
+        }
+
+
+    }
+    fun clearErrors(){
+        binding.tilUserName.error=null
+        binding.tilEmail.error=null
+        binding.tilPhoneNumber.error=null
+        binding.tilPassword.error=null
+    }
+
+
 }
